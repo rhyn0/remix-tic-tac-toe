5f8a2719209485e80447eb0c38b2e8d1fb6c07a1
Ryan Ozawa <ryan.ozawa21@gmail.com>
2024-09-01 13:08:31 -0700

    [Fixes #4] fix: coloring of home page buttons has good contrast with backgrounds in light and dark (#5)

--- app/routes/_index/route.tsx
+++ app/routes/_index/route.tsx
@@ -4,15 +4,15 @@ import { Button } from "~/components/ui/button";
 
 export default function HomeScreen() {
     return (
-        <main className="mx-auto flex flex-col w-2/3 h-1/2">
+        <main className="mx-auto flex flex-col w-2/3 h-1/2 bg-background">
             <h1 className="text-2xl text-center mb-10">
                 Welcome to Tic Tac Toe
             </h1>
             <div className="flex flex-col justify-center w-1/2 mx-auto space-y-2">
-                <Button asChild variant="link" className="dark:bg-slate-500">
+                <Button asChild variant="link" className="bg-slate-500">
                     <Link to="/new-game">New Game</Link>
                 </Button>
-                <Button asChild variant="link" className="dark:bg-slate-500">
+                <Button asChild variant="link" className="bg-slate-500">
                     <Link to="/games">Continue Game</Link>
                 </Button>
             </div>
